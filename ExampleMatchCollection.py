c6d8b4b2f7aab27a6e4b203c755c7492a9b9ee9b
Knox <knoxcrichto@gmail.com>
2025-02-11 12:53:41 -0500

    Additional testing, moving away from summoner object

--- ExampleMatchCollection.py
+++ ExampleMatchCollection.py
@@ -22,6 +22,46 @@ def filter_match_history(summoner: Summoner, patch: Patch):
     return match_history
 
 
+def collect_matches():
+    initial_summoner_name, initial_summoner_tagline = "ObKnoxious", "3824"
+    region = "NA"
+
+    account = Account(
+            name=inital_summoner_name, tagline=initial_summoner_tagline, region=region
+            )
+    summoner = account.summoner
+    patch = Patch.from_str("15.2.1", region=region)
+
+    unpulled_summoner_ids = SortedList([summoner.id])
+    pulled_summoner_ids = SortedList()
+
+    unpulled_match_ids = SortedList()
+    pulled_match_ids = SortedList()
+
+    while unpulled_summoner_ids:
+        new_summoner_id = random.choice(unpulled_summoner_ids)
+        new_summoner = Summoner(id=new_summoner_id, region=region)
+        matches = filter_match_history(new_summoner, patch)
+        unpulled_match_ids.update([match.id for match in matches])
+        unpulled_summoner_ids.remove(new_summoner_id)
+        pulled_summoner_ids.add(new_summoner_id)
+
+        while unpulled_match_ids:
+            new_match_id = random.choice(unpulled_match_ids)
+            new_match = Match(id=new_match_id, region=region)
+            for participant in new_match.participants:
+                if (
+                    participant.summoner.id not in pulled_summoner_ids
+                    and participant.summoner.id not in unpulled_summoner_ids
+                ):
+                    unpulled_summoner_ids.add(participant.summoner.id)
+            unpulled_match_ids.remove(new_match_id)
+            pulled_match_ids.add(new_match_id)
+
+
+if __name__ == "__main__":
+    collect_matches()
+
 
 
 
---
+++ basic.py
@@ -0,0 +1,9 @@
+import random
+import arrow
+import cassiopeia as cass
+
+player = cass.Account(name="ObKnoxious", tagline="3824", region="NA") 
+
+#good_with = kalturi.champion_masteries.filter(lambda cm: cm.level >= 6)
+print(player.continent, player.name, player.platform, player.account_id)
+
