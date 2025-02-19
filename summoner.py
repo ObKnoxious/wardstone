2bf389969b01b4df8e22f2eab18d8e5bb5799259
Knox <knoxcrichto@gmail.com>
2025-02-18 23:09:21 -0500

    Pulsefire can now pull match ids!

--- summoner.py
+++ summoner.py
@@ -1,6 +1,9 @@
 import asyncio
 from pulsefire.clients import RiotAPIClient
+from pulsefire.schemas import RiotAPISchema
+from pulsefire.taskgroups import TaskGroup
 from config import api_key
+
 #print("API Key: ")
 #api_key = input()
 #print(api_key)
@@ -13,7 +16,20 @@ async def main():
         account = await client.get_account_v1_by_riot_id(region="americas", game_name="ObKnoxious", tag_line="3824")
         print(account)
         summoner = await client.get_lol_summoner_v4_by_puuid(region="na1", puuid=account["puuid"])
-        assert summoner["summonerLevel"] > 200
+        match_ids = await client.get_lol_match_v5_match_ids_by_puuid(region="americas", puuid=summoner["puuid"])
+        #assert summoner["summonerLevel"] > 200
+
+        async with TaskGroup() as tg:
+            for match_id in match_ids[:20]:
+                await tg.create_task(client.get_lol_match_v5_match(region="americas", id=match_id))
+
+        matches: list[RiotApiSchema.LolMatchV5Match] = tg.results()
+
+        for match in matches:
+            assert match["metadata"]["matchId"] in match_ids
+
+
+        print(match_ids)
 
 
 asyncio.run(main())
